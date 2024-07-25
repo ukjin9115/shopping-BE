0dd268d44b21a781e72bc545d7b4f0d63636e287
ukjin9115 <dnrwls9115@naver.com>
2024-07-26 04:12:08 +0900

    로그인 기능 추가

--- src/main/java/ukgo/shop/controller/MemberController.java
+++ src/main/java/ukgo/shop/controller/MemberController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ukgo.shop.service.MemberService;
 import ukgo.shop.service.MyUserDetailsService;
-
+//로그인기능추가요
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
